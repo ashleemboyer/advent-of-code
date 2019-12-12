0c81723e776267292f23257db2026baff1330da8
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-12-11 22:12:49 -0500

    Adding D4P2 and refactoring to use chars method on password string instead of split

--- day-04/day-4.rb
+++ day-04/day-4.rb
@@ -1,18 +1,28 @@
 @min = 168630
 @max = 718098
 
-def meets_criteria? pass
+def meets_criteria?(pass, part_2=false)
   return false if pass.length != 6
 
-  digits = pass.split("")
+  sorted_digits = pass.chars.sort.join
+  return false if sorted_digits != pass
 
   has_adjacents = false
-  digits.each { |digit|
-    has_adjacents = true if pass.include?("#{digit}#{digit}")
-  }
-
-  sorted_digits = digits.sort.join
-  return false if sorted_digits != pass
+  if part_2
+    # Credit goes to Mauri Mastonen for this portion. I didn't think about
+    # using the "never decrease" rule as a precondition for this! Also
+    # thanks for helping me learn a new method: inject.
+    # Link: https://github.com/Kazhuu/advent-of-code-2019/blob/master/day04/main.rb#L25
+    digit_count = pass.chars.inject(Hash.new(0)) { |digit_to_count, digit|
+      digit_to_count[digit] += 1
+      digit_to_count
+    }
+    has_adjacents = digit_count.has_value?(2)
+  else
+    pass.chars.each { |digit|
+      has_adjacents = true if pass.include?("#{digit}#{digit}")
+    }
+  end
 
   has_adjacents
 end
@@ -25,4 +35,13 @@ def part_1
   count
 end
 
+def part_2
+  count = 0
+  (@min..@max).each { |pass|
+    count += 1 if meets_criteria?(pass.to_s, true)
+  }
+  count
+end
+
 puts "Part 1: #{part_1}"
+puts "Part 2: #{part_2}"
